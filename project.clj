ff868367e6a7911be973b3254be53e12e42ec840
南山 <junjiemars@gmail.com>
2015-01-28 16:39:33 +0800

    just run into...

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-packer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject packer "0.1.0"
+  :description "pack resource"
+  :url "https://github.com/junjiemars/lein-packer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- src/leiningen/packer.clj
+++ src/leiningen/packer.clj
@@ -1,6 +1,35 @@
-(ns leiningen.packer)
+(ns leiningen.packer
+  (:require [leiningen.help :as h]
+            [leiningen.core.main :as m]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]))
+
+(defn- once
+  [project
+   options
+   args]
+  (println "#in once"))
+
+(defn- clean
+  [project
+   options
+   args]
+  (println "#in clean"))
 
 (defn packer
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+  "Run the packer plugin."
+  {:help-arglists '([once clean])
+   :subtasks [#'once #'clean]}
+  ([project]
+   (println (h/help-for "packer"))
+   (m/abort))
+  ([project subtask & args]
+   (let [options (select-keys project [:chrome :safari])]
+     (case subtask
+       "once" (once project options args)
+       "clean" (clean project options args)
+       (do
+         (println "Subtask"
+                  (str " subtask " "not found."
+                       (h/subtask-help-for *ns* #'packer)))
+         (m/abort))))))
