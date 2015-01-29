3c35f6f0ad07ffde8226f936c2c73371c004fc83
南山 <junjiemars@gmail.com>
2015-01-29 17:19:04 +0800

    just copy files/directories

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/junjiemars/lein-packer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :dependencies [[me.raynes/fs "1.4.6"]])
--- src/leiningen/packer.clj
+++ src/leiningen/packer.clj
@@ -1,20 +1,46 @@
 (ns leiningen.packer
   (:require [leiningen.help :as h]
             [leiningen.core.main :as m]
-            [clojure.java.io :as io]
+            [me.raynes.fs :as fs]
             [clojure.pprint :refer [pprint]]))
+
+(set! *warn-on-reflection* true)
+
+(defn- find-files [re-dir]
+  (map str (fs/find-files (str (fs/parent (fs/file (str re-dir))))
+                          (re-pattern (apply str (fs/split-ext (str re-dir)))))))
+
+(defn- do-mapping [mapping]
+  (let [s (seq mapping)]
+    (doseq [{:keys [source-paths target-path]} s]
+      (let [dest (str (fs/file target-path))]
+        (doseq [sp source-paths]
+          (if (instance? java.util.regex.Pattern sp)
+            (doseq [p (find-files sp)]
+              (fs/copy-dir p dest))
+            (fs/copy-dir sp dest)))))))
 
 (defn- once
   [project
-   options
+   {{:keys [mapping target]
+     :as options} :pack}
    args]
-  (println "#in once"))
+  (println "##in once")
+  (pprint options)
+  (println "====")
+  (pprint mapping)
+  (println "====")
+  (pprint target)
+  (println "====")
+  (do-mapping mapping))
 
 (defn- clean
   [project
    options
    args]
-  (println "#in clean"))
+  (println "##in clean")
+  (pprint options)
+  (pprint args))
 
 (defn packer
   "Run the packer plugin."
@@ -24,7 +50,7 @@
    (println (h/help-for "packer"))
    (m/abort))
   ([project subtask & args]
-   (let [options (select-keys project [:chrome :safari])]
+   (let [options (select-keys project [:pack])]
      (case subtask
        "once" (once project options args)
        "clean" (clean project options args)
