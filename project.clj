89cf8536d04d724fbd1a04c406a8a8e2b46cc3d0
南山 <junjiemars@gmail.com>
2015-01-31 01:11:44 +0800

    supports lein debug/info, export DEBUG=true

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject packer "0.1.0"
-  :description "pack resource"
+  :description "pack file resources"
   :url "https://github.com/junjiemars/lein-packer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true
-  :dependencies [[me.raynes/fs "1.4.6"]])
+  :eval-in-leiningen true)
--- src/leiningen/packer.clj
+++ src/leiningen/packer.clj
@@ -8,100 +8,99 @@
 (set! *warn-on-reflection* true)
 
 (defn exists? [file]
+  "If file is exists then return it, otherwise return nil"
   (let [f (io/file file)]
     (when (.exists f)
       f)))
 
 (defn mkdir [dir]
+  "If dir is not exists or is not a directory then return it, "
+  "otherwise return it directly"
   (let [d (io/file dir)]
     (when-not (and (.exists d)
                    (.isDirectory d))
       (let [p (.getParent ^java.io.File d)]
-        (mkdir p))
-      (.mkdir d))
+        (mkdir p)
+        (.mkdir d)))
     d))
 
-(defn make-path [root leaf]
+(defn join-path [root leaf]
+  "Join root path with leaf path and return it"
   (io/file (str (.getPath ^java.io.File root)
                 (File/separator)
                 (io/file (.getName ^java.io.File leaf)))))
 
 (defn directory? [file]
+  "Is the file is directory?"
   (.isDirectory (io/file file)))
 
 (defn re-files [re]
+  "Return the file seq designated by re pattern"
   (let [f (io/file (str re))
         d (io/file (.getParent f))]
     (filter #(re-find re (.getPath ^java.io.File %)) (file-seq d))))
 
 (defn copy-file [file dir]
-  (println "@file:" file "|dir:" dir)
-  (let [n (.getName ^java.io.File (io/file file))
-        p (str (.getPath ^java.io.File dir) (File/separator) n)]
-    (io/copy file (io/file p))))
-
-(defn copy-dir-leaf [source destination]
-  (let [s (io/file source)
-        d (io/file destination)
-        l (.getName s)
-        r (.getName d)]
-    (when-not (= l r)
-      (mkdir (make-path d s)))))
+  "Copy the file to the dir"
+  (let [f (io/file file)
+        d (io/file dir)]
+    (m/debug  "#copy-file:" file " to " dir)
+    (io/copy file (join-path d f))))
 
 (defn copy-dir [source destination]
+  "Iterated copy directories and it's sub directories"
   (let [s (io/file source)
         d (mkdir destination)]
-    (println "#dd:" d)
     (doseq [f (.listFiles ^java.io.File s)]
       (if-not (directory? f)
         (copy-file f d)
-        (do
-          (println "!f:" f "|mp:" (make-path d f) "|d:" d)
-          (copy-dir f (make-path d f)))))))
+        (copy-dir f (join-path d f))))))
 
 (defn transfer [mapping]
+  "Transfer the mapping files"
   (let [s (seq mapping)]
-    (pprint (str "#mapping:" mapping))
     (doseq [{:keys [source-paths target-path]} s]
-      (println (str "%s:" source-paths "|d:" target-path))
       (let [d (mkdir target-path)]
         (doseq [p source-paths]
           (if (instance? java.util.regex.Pattern p)
             (doseq [f (re-files p)] (copy-file f d))
             (if (directory? p)
-              (do (copy-dir-leaf p d)
-                  (copy-dir p d))
+              (copy-dir p d)
               (copy-file p d))))))))
 
-(defn- once
+(defn once
   [project
    {{:keys [mapping target]
      :as options} :pack}
    args]
-  (transfer mapping))
+  (do
+    (m/debug "#once:")
+    (m/debug "#mapping:" mapping)
+    (transfer mapping)))
 
-(defn- clean
+(defn clean
   [project
    options
    args]
-  (println "##in clean")
-  (pprint options)
-  (pprint args))
+  (do
+    (m/debug "#clean:")
+    (m/debug "#options:" options)
+    (m/debug "#args:" args)))
 
 (defn packer
   "Run the packer plugin."
   {:help-arglists '([once clean])
    :subtasks [#'once #'clean]}
   ([project]
-   (println (h/help-for "packer"))
-   (m/abort))
+   (do
+     (m/info (h/help-for "packer"))
+     (m/abort)))
   ([project subtask & args]
    (let [options (select-keys project [:pack])]
      (case subtask
        "once" (once project options args)
        "clean" (clean project options args)
        (do
-         (println "Subtask"
-                  (str " subtask " "not found."
-                       (h/subtask-help-for *ns* #'packer)))
+         (m/info "Subtask "  subtask " not found."
+                 (h/subtask-help-for *ns* #'packer))
          (m/abort))))))
