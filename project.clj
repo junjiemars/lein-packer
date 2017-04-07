9047e46409ec0457ccfcd2d5e7e8616e259dd3f3
南山竹 <junjiemars@gmail.com>
2017-04-07 23:18:44 +0800

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -10,4 +10,8 @@
                                :timezone "+8"]]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories [["releases"
+                         {:url "https://clojars.org/repo"
+                          :creds :gpg}]]
+  )
